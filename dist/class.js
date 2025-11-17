17ddc347bfa473eaf7aab2cdff697bcf32083df2
Uttam <uttamrohit4545@gmail.com>
2025-11-17 22:31:50 +0600

    feat : paramter properties in typescript

--- dist/class.js
+++ dist/class.js
@@ -1,5 +1,19 @@
 "use strict";
+// class Animal {
+//     name : string;
+//     species : string;
+//     sound: string
 Object.defineProperty(exports, "__esModule", { value: true });
+//     constructor(name : string , species : string , sound : string){
+//         this.name = name;
+//         this.species = species;
+//         this.sound = sound
+//     }
+//     makeSound(){
+//         console.log(`${this.name} is sound ${this.sound}`)
+//     }
+// }
+//parameter properties
 class Animal {
     name;
     species;
--- src/class.ts
+++ src/class.ts
@@ -1,13 +1,25 @@
 
-class Animal {
-    name : string;
-    species : string;
-    sound: string
+// class Animal {
+//     name : string;
+//     species : string;
+//     sound: string
 
-    constructor(name : string , species : string , sound : string){
-        this.name = name;
-        this.species = species;
-        this.sound = sound
+//     constructor(name : string , species : string , sound : string){
+//         this.name = name;
+//         this.species = species;
+//         this.sound = sound
+//     }
+
+//     makeSound(){
+//         console.log(`${this.name} is sound ${this.sound}`)
+//     }
+// }
+
+
+//parameter properties
+class Animal {
+    constructor(public name : string ,public species : string ,public sound : string){
+        
     }
 
     makeSound(){
