52f675270216de296f416d0f2f9bf344ac950561
Uttam <uttamrohit4545@gmail.com>
2025-11-18 12:42:49 +0600

    feat : Abstraction

---
+++ src/Abstraction.ts
@@ -0,0 +1,57 @@
+//amra abstraction banate pari interface ba abstract class use kore
+
+
+// interface MediaPlayer {
+//     play() :void;
+//     pause():void;
+//     stop():void
+// }
+
+
+// class MusicPlayer implements MediaPlayer {
+//     play(): void {
+//         console.log("Playing Music...")
+//     }
+
+//     pause(): void {
+//         console.log("Pause music...")
+//     }
+//     stop(): void {
+//         console.log("Stop Music...")
+//     }
+// }
+
+
+// const instance1 = new MusicPlayer()
+// instance1.play()
+
+
+
+abstract class MediaPlayer {
+    abstract play() :void;
+    abstract pause() :void;
+    abstract stop():void;
+
+    Welcome(){
+        console.log("Welcome out media player and enjoy")
+    }
+}
+
+class MusicPlayer extends MediaPlayer {
+    play(): void {
+        console.log("Music playing...")
+    }
+
+    pause(): void {
+        console.log("Music pause...")
+    }
+
+    stop(): void {
+        console.log("Music is stop")
+    }
+}
+
+
+const instance1 = new MusicPlayer()
+instance1.Welcome()
+instance1.play()
